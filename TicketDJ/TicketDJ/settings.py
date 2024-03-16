674c6a873c977244476d528422da1d1f6cdd1b84
lsands-washburn <157529799+lsands-washburn@users.noreply.github.com>
2024-03-16 06:17:45 -0500

    tickets app & homepage

--- TicketDJ/TicketDJ/settings.py
+++ TicketDJ/TicketDJ/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mainapp', #1st priotity app
+    'tickets',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ TicketDJ/tickets/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ticket_list(request):
+    return render(request,'tickets/ticket_list.html')
