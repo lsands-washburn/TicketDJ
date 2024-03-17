89951bec2064d57ff8653eb5444f1d570e4c3018
Jefferw <jefferson.williams@washburn.edu>
2024-03-17 18:47:33 -0500

    add notes to ticket

--- TicketDJ/tickets/urls.py
+++ TicketDJ/tickets/urls.py
@@ -6,6 +6,7 @@ app_name = 'tickets'
 urlpatterns = [
     path('mainapp/', include('mainapp.urls')),
     path('ticket-list/', views.ticket_list, name='ticket_list'),
-    path('ticket/<ticket_id>/', views.ticket_detail, name='ticket_detail'),  # Choose a specific path for the ticket list view
+    path('ticket/<ticket_id>/', views.ticket_detail, name='ticket_detail'),
+    path('add_note/<ticket_id>/', views.add_note, name='add_note')  # Choose a specific path for the ticket list view
 ]
 
--- TicketDJ/tickets/views.py
+++ TicketDJ/tickets/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import pyodbc
+from django.http import HttpResponseRedirect
+import uuid
 
 @login_required
 def ticket_list(request):
@@ -61,3 +63,29 @@ def ticket_detail(request, ticket_id):
     cursor.close()
     conn.close()
     return render(request, 'tickets/ticket_detail.html', {'ticket': ticket, 'notes': notes})
+
+@login_required
+def add_note(request, ticket_id):
+    db_settings = {
+        'driver': '{ODBC Driver 17 for SQL Server}',
+        'server': 'cm465.database.windows.net',
+        'database': 'HelpdeskProject',
+        'user': 'CM465Admin@cm465',
+        'password': 'CM465Password!',
+    }
+
+    # Establish database connection
+    conn = pyodbc.connect(
+        f"DRIVER={db_settings['driver']};SERVER={db_settings['server']};DATABASE={db_settings['database']};UID={db_settings['user']};PWD={db_settings['password']}"
+    )
+    cursor = conn.cursor()
+
+    note_text = request.POST.get('note')
+    note_id = "N" + str(uuid.uuid4())[:9]
+    cursor.execute("INSERT INTO Note (note_id, note, ticket_id) VALUES (?, ?, ?)", note_id, note_text, ticket_id)
+
+    conn.commit()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
