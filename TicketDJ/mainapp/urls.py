47750f57803114ed3f75a5290c314d478090b10d
lsands-washburn <157529799+lsands-washburn@users.noreply.github.com>
2024-03-26 02:57:53 -0500

    Ticket create functionality
    added create ticket function that redirects to the ticket create page with ticket details. Also made the dashboard function work within the ticket app instead of mainapp/registration

--- TicketDJ/mainapp/urls.py
+++ TicketDJ/mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from tickets.views import dashboard
 
 urlpatterns = [
     path('',views.dashboard,name='dashboard'),
