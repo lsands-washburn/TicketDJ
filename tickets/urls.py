623e646567bf69668fd4f957186abe50265b290a
lsands-washburn <157529799+lsands-washburn@users.noreply.github.com>
2024-04-08 08:04:58 -0500

    added admin panel
    - if user.is_staff, the user can see all tickets in ticket list, has same abilities as tech, has access to admin panel button
    -admin panel url will reject user if not staff
    - preliminary admin panel displays stats about users/tickets

---
+++ tickets/urls.py
@@ -0,0 +1,19 @@
+from django.urls import include, path
+from . import views
+
+app_name = 'tickets'
+
+urlpatterns = [
+    path('mainapp/', include('mainapp.urls')),
+    path('ticket-list/', views.ticket_list, name='ticket_list'),
+    path('ticket/<ticket_id>/', views.ticket_detail, name='ticket_detail'),
+    path('add_note/<ticket_id>/', views.add_note, name='add_note'),  
+    path('create-ticket/', views.create_ticket, name='create_ticket'),
+    path('ticket_created/', views.ticket_created, name='ticket_created'),
+    path('assign_ticket/', views.assign_ticket, name='assign_ticket'),
+    path('modify_ticket/<ticket_id>/', views.modify_ticket, name='modify_ticket'),
+    path('ticket/<ticket_id>/update/', views.update_ticket, name = 'update_ticket'),
+    path('admin_panel/', views.admin_panel, name='admin_panel'),
+    
+]
+
