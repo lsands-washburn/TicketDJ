3d199d39665065e9a9631413e178d7e271cbf21b
lsands-washburn <157529799+lsands-washburn@users.noreply.github.com>
2024-04-17 18:34:30 -0500

    updated add_user.py to write records to SSMS

--- mainapp/management/commands/add_users.py
+++ mainapp/management/commands/add_users.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User, Group
 from faker import Faker
+import pyodbc
+from mainapp.views import db_connect
 
 class Command(BaseCommand):
     help = 'Adds 70 users with pseudo-realistic information'
@@ -13,6 +15,8 @@ class Command(BaseCommand):
         except Group.DoesNotExist:
             self.stdout.write(self.style.ERROR('Group {} does not exist'.format(group_name)))
             return
+        conn = db_connect()
+        cursor = conn.cursor()
         for _ in range(70):
             username = fake.user_name()
             first_name = fake.first_name()
@@ -21,4 +25,6 @@ class Command(BaseCommand):
             password = 'password123'  #set a common password for all users
             user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
             user.groups.add(group)  # Add user to the group
+            cursor.execute("""INSERT INTO Helpdesk_User (username, email, first_name, last_name) VALUES (?, ?, ?, ?);""", [username, email, first_name, last_name])
+            conn.commit()
         self.stdout.write(self.style.SUCCESS('Successfully added 70 users to group {}'.format(group_name)))
