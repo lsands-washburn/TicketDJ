cce683b05ca945ae5363e9791ba762f0515d6e6e
lsands-washburn <157529799+lsands-washburn@users.noreply.github.com>
2024-04-16 22:57:33 -0500

    make migrations and cleanup for last commit

--- mainapp/management/commands/add_users.py
+++ mainapp/management/commands/add_users.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from faker import Faker
 
 class Command(BaseCommand):
@@ -7,11 +7,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         fake = Faker()
+        group_name = 'EndUser'  #desired group name
+        try:
+            group = Group.objects.get(name=group_name)
+        except Group.DoesNotExist:
+            self.stdout.write(self.style.ERROR('Group {} does not exist'.format(group_name)))
+            return
         for _ in range(70):
             username = fake.user_name()
             first_name = fake.first_name()
             last_name = fake.last_name()
             email = fake.email()
-            password = 'password123'  #common password for all users
-            User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
-        self.stdout.write(self.style.SUCCESS('Successfully added 70 users'))
+            password = 'password123'  #set a common password for all users
+            user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
+            user.groups.add(group)  # Add user to the group
+        self.stdout.write(self.style.SUCCESS('Successfully added 70 users to group {}'.format(group_name)))
